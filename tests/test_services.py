3a5418d56078bb7fdd2468e2ef79a9c21f2c5b36
rejinjosek <rejinjose.k@gmail.com>
2024-03-24 22:31:23 +0530

    modified tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,27 +1,14 @@
-"""
-return subfeddits of given skip and limit values
-url = "http://localhost:8080/api/v1/subfeddits/?skip=2&limit=10"
-response: ['limit', 'skip', 'subfeddits']
-subfeddits: [{'id': 3, 'username': 'admin_3', 'title': 'Dummy Topic 3', 'description': 'Dummy Topic 3'}]
-
-return subfeddit for given ID
-url = "http://localhost:8080/api/v1/subfeddit/?subfeddit_id=2"
-Id is mandatory field
-response:['id', 'username', 'title', 'description', 'limit', 'skip', 'comments']
-comments: ['id', 'username', 'text', 'created_at'] unix epochs
-
-Return comments of given subfeddit ID with skip and limit range
-http://localhost:8080/api/v1/comments/?subfeddit_id=2&skip=0&limit=10
-response: ['subfeddit_id', 'limit', 'skip', 'comments']
-commentsList[dict]: ['id', 'username', 'text', 'created_at']
-
-
-"""
 from app.services import get_subfeddit_by_title, SentimentAnalyzer
 
+import pytest
+
+
+present_title = "Dummy Topic 1"
+absent_title = "Rejin"
+skip_records = 0
+limit_records = 2
 
 def test_get_subfeddit_by_title_present_title():
-    present_title = "Dummy Topic 1"
     present_subfeddit = get_subfeddit_by_title(present_title)
     assert isinstance(present_subfeddit,dict)
     assert present_subfeddit.get('title')==present_title
@@ -32,17 +19,15 @@ def test_get_subfeddit_by_title_absent_title():
     assert isinstance(absent_subfeddit, str)
 
 def test_sentiment_analyzer_get_scores():
-    subfeddit_title = "Dummy Topic 1"
-    skip_records = 0
-    limit_records = 2
-    sa = SentimentAnalyzer(subfeddit_title, skip_records, limit_records)
-    scores = sa.get_scores()
+    sa = SentimentAnalyzer()
+    scores = sa.get_scores(present_title, limit_records, skip_records, sort_by_scores=True)
     
     assert len(scores)==2
     assert 'type' in scores[0].keys()    
+    # Test sorting
+    assert all(scores[i]["score"] <= scores[i + 1]["score"] for i in range(len(scores) - 1))
 
 def test_sentiment_analyzer_get_scores_absent_title():
-    subfeddit_title = "Rejin"
     sa = SentimentAnalyzer()
-    scores = sa.get_scores(subfeddit_title)
-    assert isinstance(scores,str)
+    with pytest.raises(ValueError):
+        sa.get_scores(absent_title)
